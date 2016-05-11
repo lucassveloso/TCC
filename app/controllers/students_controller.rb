c0b79a9ae1c177c38323e787f6f258c365d25499
lucassveloso <lucas.s.veloso@hotmail.com>
2016-05-11 01:18:23 -0300

    #1 Criando layout e funcionalidade básica da busca e listagem de alunos

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,6 +1,10 @@
 class StudentsController < ApplicationController
 
   def index
-    @students_result = Student.all
+    if params[:text_search].blank?
+       @student_search_results = Student.all.includes(:person)
+    else
+       @student_search_results = Student.search(params[:text_search]).includes(:person)
+    end
   end
 end
