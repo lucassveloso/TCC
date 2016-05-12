3ef5485f48dcb2ac86dae32e12aeb8a2f014d229
lucassveloso <lucas.s.veloso@hotmail.com>
2016-05-11 21:30:59 -0300

    #0 - Criando seeds de db

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+address = Address.create(address:'xyz Street', zipcode:'00000-000', city:'City', state:'State', country:'Country', neighborhood: 'Neighborhood', complement: 'Complement', number: 123)
+certificate = Certificate.create(type_of:'birth', term_number: 123, sheet_number: 321, book: "A", emission_date: "01/01/2000", federation_unit: "RS",  notarys_office: "notary's office")
+identity_document = IdentityDocument.create(identity_number: 123, dispatch_date: "01/01/2000", federation_unit: "RS",  dispatcher_organ: "SSP")
+person_father = Person.create(name: "João Father", genre: "Male", birthdate: "22/08/1965", race: "white", phone: "12345", cellphone: "54321", naturalness: "Viamão, RS", nationality: "brasileira", address: address)
+person_son = Person.create(name: "Lucas Son", genre: "Male", birthdate: "22/08/1995", race: "white", phone: "12345", cellphone: "54321", naturalness: "Viamão, RS", nationality: "brasileira", address: address)
+student = Student.create(person: person_son, nis_number: 123, certificate: certificate, identity_document: identity_document, cpf: "000.000.000-00", entry_date: "01/01/2000", special_needs: "Down's syndrome", teaching_step: "EJA", photo_url: "www.photo.com")
+person_daughter = Person.create(name: "Maria Daughter", genre: "Female", birthdate: "22/08/1995", race: "white", phone: "12345", cellphone: "54321", naturalness: "Viamão, RS", nationality: "brasileira", address: address)
+student2 = Student.create(person: person_daughter, nis_number: 123, certificate: certificate, identity_document: identity_document, cpf: "000.000.000-00", entry_date: "01/01/2000", special_needs: "Down's syndrome", teaching_step: "EJA", photo_url: "www.photo.com")
+responsible = Responsible.create(person: person_father, kinship: "Father", students: [student, student2])
+
