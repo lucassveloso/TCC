d2756c37ab52db9fa5f9e1b1acbcbded3d9a1f54
Lucas Veloso <lucas.s.veloso@hotmail.com>
2016-06-09 19:53:47 -0300

    #0 - Refatorando máscara de datas para funcionar com o datepicker do HTML5 no Chrome

--- app/assets/javascripts/masks.js
+++ app/assets/javascripts/masks.js
@@ -1,10 +1,14 @@
 var MASKS = MASKS || {}
 
 MASKS.addMasks = function addMasks() {
-  $('.date-mask').mask("99/99/9999");
+  $('.date-mask').mask(MASKS.isGoogleChrome() ? "9999-99-99" : "99/99/9999");
   $('.cep-mask').mask('00000-000');
   $('.phone-mask').mask('(00)0000-0000');
   $('.cpf-mask').mask('000.000.000-00', {
     reverse: true
   });
+}
+
+MASKS.isGoogleChrome = function isGoogleChrome() {
+  return (/Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor));
 }
