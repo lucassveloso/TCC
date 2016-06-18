20d41b8e8e1f1e15ff4414a083240e769ae5d57a
lucassveloso <lucas.s.veloso@hotmail.com>
2016-06-18 01:55:24 -0300

    #3 - Aplicando destroy em castata dos objetos dependentes de aluno

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,6 +1,6 @@
 class Document < ActiveRecord::Base
-  belongs_to :identity_document
-  belongs_to :certificate
+  belongs_to :identity_document, dependent: :destroy
+  belongs_to :certificate, dependent: :destroy
   has_one :person
   accepts_nested_attributes_for :identity_document, :certificate
 end
