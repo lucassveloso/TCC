da6f2c8311eae862eb0a0a9aec4b1c831460fd05
lucassveloso <lucas.s.veloso@hotmail.com>
2016-07-03 15:49:28 -0300

    #6 - Corrigindo teste de validações de responsáveis

--- spec/models/guardian_spec.rb
+++ spec/models/guardian_spec.rb
@@ -3,21 +3,11 @@ require 'rails_helper'
 describe Guardian, type: :model do
   describe 'validation' do
     before :each do
-      @guardian = Guardian.new(person: Person.new, kinship: "Mother", students: [Student.new])
+      @guardian = Guardian.new(person: Person.new)
     end
 
     it 'should require a person' do
       @guardian.person = nil
-      expect(@guardian).to_not be_valid
-    end
-
-    it 'should require a kinship' do
-      @guardian.kinship = nil
-      expect(@guardian).to_not be_valid
-    end
-
-    it 'should require students' do
-      @guardian.students = []
       expect(@guardian).to_not be_valid
     end
 
